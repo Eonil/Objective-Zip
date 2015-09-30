9ffbde1cfc3652de7147675ef16c2f5971e8bfeb
Hoon H <drawtree@me.com>
2015-10-01 01:22:30 +0900

    Added dynamic framework target.

---
+++ ObjectiveZip/ObjectiveZip.h
@@ -0,0 +1,14 @@
+//
+//  ObjectiveZip.h
+//  Objective-Zip
+//
+//  Created by Hoon H. on 2015/10/01.
+//
+//
+
+#ifndef ObjectiveZip_h
+#define ObjectiveZip_h
+
+#import "Objective-Zip+NSError.h"
+
+#endif /* ObjectiveZip_h */
